9bb1c7c7596622ec82e22d63b534fd37fd22df09
AndyPan <panjf2000@gmail.com>
2016-12-28 12:22:02 +0800

    add init config for cache

---
+++ cache/init.go
@@ -0,0 +1,30 @@
+package cache
+
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/panjf2000/goproxy/config"
+	"os"
+)
+
+var conf config.Config
+
+//this method will initialize a log module
+func initLog() {
+	// Log as JSON instead of the default ASCII formatter.
+	logrus.SetFormatter(&logrus.TextFormatter{})
+
+	// Output to stderr instead of stdout, could also be a file.
+	logrus.SetOutput(os.Stderr)
+
+	// Only log the warning severity or above.
+	logrus.SetLevel(logrus.DebugLevel)
+
+}
+
+func init() {
+	err := conf.GetConfig("config/config.json")
+	initLog()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+}
