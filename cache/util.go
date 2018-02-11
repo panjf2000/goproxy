ae51b8eb39085a9a8049b44c7d5bb8ff7fdc01a5
andy pan <panjf2000@gmail.com>
2018-02-11 19:02:40 +0800

    pring http header in log

--- cache/util.go
+++ cache/util.go
@@ -1,14 +1,16 @@
 package cache
 
 import (
+	"log"
 	"net/http"
 	"strings"
 )
 
 //checks whether request ask to be stored as cache
-func IsReqCache(resp *http.Request) bool {
-	cacheControl := resp.Header.Get("Cache-Control")
-	contentType := resp.Header.Get("Content-Type")
+func IsReqCache(req *http.Request) bool {
+	log.Printf("http request header:%v", req.Header)
+	cacheControl := req.Header.Get("Cache-Control")
+	contentType := req.Header.Get("Content-Type")
 	if cacheControl == "" && contentType == "" {
 		return true
 	} else if len(cacheControl) > 0 {
@@ -18,9 +20,9 @@ func IsReqCache(resp *http.Request) bool {
 			strings.Index(cacheControl, "must-revalidate") != -1 ||
 			(strings.Index(cacheControl, "max-age") == -1 &&
 				strings.Index(cacheControl, "s-maxage") == -1 &&
-				resp.Header.Get("Etag") == "" &&
-				resp.Header.Get("Last-Modified") == "" &&
-				(resp.Header.Get("Expires") == "" || resp.Header.Get("Expires") == "0")) {
+				req.Header.Get("Etag") == "" &&
+				req.Header.Get("Last-Modified") == "" &&
+				(req.Header.Get("Expires") == "" || req.Header.Get("Expires") == "0")) {
 			return false
 		}
 
@@ -37,6 +39,7 @@ func IsReqCache(resp *http.Request) bool {
 
 //checks whether response can be stored as cache
 func IsRespCache(resp *http.Response) bool {
+	log.Printf("http response header:%v", resp.Header)
 	cacheControl := resp.Header.Get("Cache-Control")
 	contentType := resp.Header.Get("Content-Type")
 	if cacheControl == "" && contentType == "" {
